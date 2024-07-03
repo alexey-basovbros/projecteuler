fbfeb04325ea4b080e9b593618423a4f74e828af
Alexey Basov <alexey@basovbros.io>
2024-07-03 23:56:54 +0300

    fix app fc return value

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,8 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactElement } from 'react';
 
 interface AppProps {};
 
-const App: FC<AppProps> = (): ReactNode => {
+const App: FC<AppProps> = (): ReactElement => {
   return <div className='app-wrapper'>
     <h3>App component</h3>
   </div>
