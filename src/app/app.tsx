23015a37aca280c5487bb683549c769b3e84f953
Alexey Basov <alexey@basovbros.io>
2024-07-04 12:50:12 +0300

    fix app test component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,7 +3,7 @@ import { FC, ReactElement } from 'react';
 interface AppProps {};
 
 const App: FC<AppProps> = (): ReactElement => {
-  return <div className='app-wrapper'>
+  return <div data-testid="app-wrapper" className='app-wrapper'>
     <h3>App component</h3>
   </div>
 };
